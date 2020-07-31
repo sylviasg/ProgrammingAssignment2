cccb2597047677071e736a2cb0b47b6b4d1bfe66
sylviasg <sylviasg@gmail.com>
2020-07-31 03:14:22 -0400

    completed code

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        if (nrow (x)!= ncol(x))
+        {
+                print ("not a square matrix")
+                x = matrix() 
+        }
+        setmatrix <- function (mtrx)
+        {
+                if (nrow (mtrx)== ncol(mtrx))
+                {
+                        x <<- mtrx
+                        inv <<- NULL
+                }
+                else
+                {
+                        print ("not a square matrix")
+                        x = matrix()
+                }
+                
+        }
+        getmatrix <- function()
+        {
+                x
+        }
+        setinverse <- function(inverse)
+        {
+                inv <<- inverse
+        }
+        getinverse <- function()
+        {
+                inv
+        }
+        list (set=setmatrix, get = getmatrix, setinv = setinverse, getinv = getinverse)
 }
 
 
@@ -12,4 +44,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cacheinv <- x$getinv()
+        if (!is.null(cacheinv))
+        {
+                message ("using cached value of inverse")
+                
+        }
+        else
+        {
+                mymatrix <-x$get()
+                cacheinv <- solve(mymatrix)
+                x$setinv(cacheinv)
+                
+        }
+        cacheinv
 }
+S
